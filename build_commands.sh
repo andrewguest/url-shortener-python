6d7496c91aa4da430b1548e26f9afd04453aeba6
Andrew <andrew@aguest.me>
2025-03-30 15:21:38 -0500

    Try using uv

--- build_commands.sh
+++ build_commands.sh
@@ -4,7 +4,12 @@ set -e
 
 # Install OS dependencies
 apt-get update && \
-apt-get install -y cargo
+apt-get install -y curl build-essential
+
+# Install uv
+curl -LsSf https://astral.sh/uv/install.sh | sh && \
+. "$HOME/.local/bin/env" && \
+uv sync
 
 # Install Python dependencies
-pip install -r requirements.txt
+# pip install -r requirements.txt
